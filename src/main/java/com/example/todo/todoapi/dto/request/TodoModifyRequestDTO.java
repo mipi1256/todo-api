f2da76900918c80a5d3badcc2940ebc4da75f50f
mipi1256 <mipi1258@gmail.com>
2024-05-17 12:09:04 +0900

    #2. todo api 할 일 기능 작성 완료

---
+++ src/main/java/com/example/todo/todoapi/dto/request/TodoModifyRequestDTO.java
@@ -0,0 +1,63 @@
+package com.example.todo.todoapi.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoModifyRequestDTO {
+
+   @NotBlank
+   private String id;
+
+   private boolean done;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.todo.todoapi.service;
 
 import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
 import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
 import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
 import com.example.todo.todoapi.entity.Todo;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -39,6 +40,29 @@ public class TodoService {
             .todos(dtoList)
             .build();
 
+   }
+
+
+   public TodoListResponseDTO delete(final String todoId) throws Exception {
+      try {
+         todoRepository.deleteById(todoId);
+      } catch (Exception e) {
+         log.error("ID가 존재하지 않아 삭제에 실패했습니다. - ID: {}, err: {}", todoId, e.getMessage());
+         throw new RuntimeException("id가 존재하지 않아 삭제에 실패했습니다");
+      }
+
+      return retrieve();
+   }
+
+   public TodoListResponseDTO update(TodoModifyRequestDTO requestDTO) throws Exception {
+      Optional<Todo> targetEntity = todoRepository.findById(requestDTO.getId());
+
+      targetEntity.ifPresent(todo -> {
+         todo.setDone(requestDTO.isDone());
+         todoRepository.save(todo);
+      });
+
+      return retrieve();
    }
 
 
