940b9c86a0b851d92855fff0aee0d41a0aa85d66
mipi1256 <mipi1258@gmail.com>
2024-05-17 17:17:19 +0900

    #3. user api 기본 구성, 이메일 중복 확인 요청 처리

---
+++ src/main/java/com/example/todo/userapi/entity/User.java
@@ -0,0 +1,82 @@
+package com.example.todo.userapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_user")
+public class User {
+
+   @Id
+   @Column(name = "user_id")
+   @GeneratedValue(strategy = UUID)
+   private String id;
+
+   @Column(nullable = false, unique = true)
+   private String email;
+
+   @Column(nullable = false)
+   private String password;
+
+   @Column(nullable = false)
+   private String userName;
+
+   @CreationTimestamp
+   private LocalDateTime joinDate;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
